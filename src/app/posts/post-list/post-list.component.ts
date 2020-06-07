76f10eb26766a7e44e8e962db1ac809469631614
ElzaCS <anuejohn@gmail.com>
2020-06-07 18:30:07 +0530

    inform next deadline

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -14,6 +14,8 @@ export class PostListComponent implements OnInit, OnDestroy{
   //   {title:'Second Post', content:'This is the second post\'s content'},
   //   {title:'Third Post', content:'This is the third post\'s content'}
   // ]
+date: Date;
+message: string;
 posts: Posts[]=[];
 private postSub: Subscription;
 postsWork: Posts[]=[];
@@ -22,10 +24,16 @@ postsPersonal: Posts[]=[];
 private postSubPersonal: Subscription;
   constructor(public post:PostService){}
   ngOnInit(){
+    this.date=new Date();
     this.post.getPosts();
     this.postSub=this.post.getPostUpdateListener().subscribe((posts: Posts[])=>{
       this.posts=posts;
       console.log(JSON.stringify(this.posts));
+      if (this.date>this.posts[0].duedate)
+      this.message="Your next task is due in "+this.posts[0].duedate;
+    else
+      this.message="Your next task is overdue from "+this.posts[0].duedate;
+    console.log("message"+ this.message);
     });
 
   }
